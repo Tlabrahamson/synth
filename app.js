7a3b3976f6af8dfa576ad3e69c47acc82c5c639e
Tim Abrahamson <tim@kinetic.com>
2020-06-08 15:59:06 -0500

    Added some JavaScript animations

---
+++ app.js
@@ -0,0 +1,49 @@
+// Declarations
+let controller;
+let sectionScene;
+let pageScene;
+
+function animateSections() {
+  // Initiate controller
+  controller = new ScrollMagic.Controller();
+  // Select the sections
+  const sliders = document.querySelectorAll(".page-section");
+
+  // Loop over each of the sections
+  sliders.forEach((section, index, sections) => {
+    const image = section.querySelector("img");
+    const reveal = section.querySelector(".text-reveal");
+
+    // GSAP
+    const sectionTimeline = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut" }
+    });
+
+    sectionTimeline.fromTo(image, { x: "-120%" }, { x: "0%" });
+    sectionTimeline.fromTo(image, { opacity: 0 }, { opacity: "1" }, "-=0.75");
+    sectionTimeline.fromTo(reveal, { left: "0%" }, { left: "120%" }, "-=1");
+
+    // Create a scene
+    sectionScene = new ScrollMagic.Scene({
+      triggerElement: section,
+      reverse: false,
+      triggerHook: 0.75
+    })
+      .setTween(sectionTimeline)
+      .addTo(controller);
+
+    // New Animation
+    const pageTimeline = gsap.timeline();
+    pageTimeline.fromTo(section, { opacity: 1 }, { opacity: 0 });
+    // Create the page scene
+    pageScene = new ScrollMagic.Scene({
+      triggerElement: section,
+      duration: "100%",
+      triggerHook: 0
+    })
+      .setTween(pageTimeline)
+      .addTo(controller);
+  });
+}
+
+animateSections();
