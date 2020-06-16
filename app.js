150c6ce49f269bd5e41bde4d2622da52bfe081f7
Tim Abrahamson <tim@kinetic.com>
2020-06-15 22:29:22 -0500

    Finished JavaScript

--- app.js
+++ app.js
@@ -18,13 +18,12 @@ function animateSections() {
   // Initiate controller
   controller = new ScrollMagic.Controller();
   // Select the sections
-  const sections = document.querySelectorAll("section");
+  const sectionSliders = document.querySelectorAll(".page-section");
 
   // Loop over each of the sections
-  sections.forEach((section, index, sections) => {
+  sectionSliders.forEach(section => {
     const image = section.querySelectorAll("img");
     const reveal = section.querySelector(".text-reveal");
-    const form = section.querySelector("form");
 
     // GSAP
     const sectionTimeline = gsap.timeline({
@@ -34,7 +33,6 @@ function animateSections() {
     sectionTimeline.fromTo(image, { x: "-120%" }, { x: "0%" });
     sectionTimeline.fromTo(image, { opacity: 0 }, { opacity: "1" }, "-=0.75");
     sectionTimeline.fromTo(reveal, { left: "0%" }, { left: "120%" }, "-=0.75");
-    sectionTimeline.fromTo(form, { opacity: 0 }, { opacity: "1" }, "-=1.5");
 
     // Create a scene
     sectionScene = new ScrollMagic.Scene({
@@ -57,6 +55,53 @@ function animateSections() {
       .setTween(pageTimeline)
       .addTo(controller);
   });
+
+  const zooImages = document.querySelectorAll(".grid-item");
+
+  zooImages.forEach(section => {
+    // GSAP
+    const image = section.querySelectorAll("img");
+    const textReveal = section.querySelectorAll(".grid-section-reveal");
+
+    const zooTimeline = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut" }
+    });
+
+    zooTimeline.fromTo(
+      image,
+      { x: "300%", opacity: 0 },
+      { x: "0%", opacity: 1 },
+      "-=1.5"
+    );
+    zooTimeline.fromTo(textReveal, { x: "0%" }, { x: "300%" }, "-=1.5");
+
+    // Create a scene
+    zooScene = new ScrollMagic.Scene({
+      triggerElement: section,
+      reverse: false,
+      triggerHook: 0.75
+    })
+      .setTween(zooTimeline)
+      .addTo(controller);
+  });
+
+  const form = document.querySelector(".form-section");
+
+  // GSAP
+  const formTimeline = gsap.timeline({
+    defaults: { duration: 1, ease: "power2.inOut" }
+  });
+
+  formTimeline.fromTo(form, { opacity: 0 }, { opacity: 1 }, "-=1.5");
+
+  // Create a scene
+  formScene = new ScrollMagic.Scene({
+    triggerElement: form,
+    reverse: false,
+    triggerHook: 0.75
+  })
+    .setTween(formTimeline)
+    .addTo(controller);
 }
 
 animateSections();
